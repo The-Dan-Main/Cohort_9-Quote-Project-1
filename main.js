ea9ba10703b72f6ce589b8c4ac539dcf58de2de2
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-08-11 18:13:41 +0200

    added BG and Text format functions

--- main.js
+++ main.js
@@ -6,12 +6,56 @@ const textLayoutSwitchButtons = document.getElementsByClassName("textLayoutSwitc
 const textFormatSwitchButtons = document.getElementsByClassName("textFormatSwitch");
 const randomBackgorundSwitchButton = document.querySelector(".randomBackgorundSwitch")
 const fontSelect = document.querySelector("select")
-
-console.log(fontColorSwitchButtons)
-console.log(backgroundColorSwitchButtons)
-console.log(textLayoutSwitchButtons)
-console.log(textFormatSwitchButtons)
-console.log(randomBackgorundSwitchButton)
-console.log(fontSelect)
+const quoteContainer = document.querySelector("#quote-container")
 
 
+
+// console.log(fontColorSwitchButtons)
+// console.log(backgroundColorSwitchButtons)
+// console.log(textLayoutSwitchButtons)
+// console.log(textFormatSwitchButtons)
+// console.log(randomBackgorundSwitchButton)
+// console.log(fontSelect)
+
+
+const changeBGColor = (event) => {
+    quoteContainer.style.backgroundColor = event.target.title
+    if (event.target.title == "black") {
+        quoteContainer.style.color = "white"
+    }
+    if (event.target.title == "white") {
+        quoteContainer.style.color = "black"
+    }
+}
+
+const changeTextFormat = (event) => {
+    const direction = event.target.title
+    console.log(direction)
+    quoteContainer.classList.remove("right", "left", "center")
+    quoteContainer.classList.toggle(direction)
+    console.log(quoteContainer.style.alignItems)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (let button of textLayoutSwitchButtons) {
+    button.addEventListener("click", changeTextFormat)
+ }
+
+
+ for (let button of backgroundColorSwitchButtons) {
+    button.addEventListener("click", changeBGColor)
+ }
