9958d6a5e9807d6b0fa6b15343b64bf571ad8669
The-Dan-Main <104311440+The-Dan-Main@users.noreply.github.com>
2022-08-11 20:14:46 +0200

    added new color palette

--- main.js
+++ main.js
@@ -4,58 +4,72 @@ const fontColorSwitchButtons = document.getElementsByClassName("fontColorSwitch"
 const backgroundColorSwitchButtons = document.getElementsByClassName("backgroundColorSwitch");
 const textLayoutSwitchButtons = document.getElementsByClassName("textLayoutSwitch");
 const textFormatSwitchButtons = document.getElementsByClassName("textFormatSwitch");
-const randomBackgorundSwitchButton = document.querySelector(".randomBackgorundSwitch")
+const formatReset = document.querySelector(".randomBackgorundSwitch")
 const fontSelect = document.querySelector("select")
 const quoteContainer = document.querySelector("#quote-container")
 
 
 
-// console.log(fontColorSwitchButtons)
-// console.log(backgroundColorSwitchButtons)
-// console.log(textLayoutSwitchButtons)
-// console.log(textFormatSwitchButtons)
-// console.log(randomBackgorundSwitchButton)
-// console.log(fontSelect)
-
 
 const changeBGColor = (event) => {
     quoteContainer.style.backgroundColor = event.target.title
-    if (event.target.title == "black") {
-        quoteContainer.style.color = "white"
-    }
-    if (event.target.title == "white") {
-        quoteContainer.style.color = "black"
-    }
 }
 
 const changeTextFormat = (event) => {
     const direction = event.target.title
-    console.log(direction)
     quoteContainer.classList.remove("right", "left", "center")
     quoteContainer.classList.toggle(direction)
-    console.log(quoteContainer.style.alignItems)
+}
+
+
+const changeTextLayout = (event) => {
+    const shape = event.target.title
+    quoteContainer.classList.remove("format-square", "format-wide", "format-high")
+    quoteContainer.classList.toggle(shape)
+}
+
+
+const resetStyles = () => {
+    quoteContainer.classList.remove("format-square", "format-wide", "format-high", "right", "left", "center");
+    quoteContainer.style = {
+        color: "black",
+        backgroundColor: "var(--light-green)",
+        fontFamily: "'Work Sans', sans-serif"
+    }
+    fontSelect.options.selectedIndex = 0;
 }
 
 
 
+const selectFont = (event) => {
+    console.log(event.target.value)
+    quoteContainer.style.fontFamily = event.target.value;
+}
+
+const changeFontColor = (event) => {
+    quoteContainer.style.color = event.target.title
+}
 
 
 
+// Eventlisteners
+formatReset.addEventListener("click", resetStyles)
 
-
-
-
-
-
-
-
-
+for (let button of textFormatSwitchButtons) {
+    button.addEventListener("click", changeTextLayout)
+}
 
 for (let button of textLayoutSwitchButtons) {
     button.addEventListener("click", changeTextFormat)
- }
+}
 
 
- for (let button of backgroundColorSwitchButtons) {
+for (let button of backgroundColorSwitchButtons) {
     button.addEventListener("click", changeBGColor)
- }
+}
+
+for (let button of fontColorSwitchButtons) {
+    button.addEventListener("click", changeFontColor)
+}
+
+fontSelect.addEventListener("change", selectFont)
